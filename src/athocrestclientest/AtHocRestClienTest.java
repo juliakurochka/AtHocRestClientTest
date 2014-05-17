c9cdca9f2a77bd2d4018e74fc541cfda34143c73
alexkurochka <alex.kurocha@gree.co.jp>
2014-05-16 20:09:14 -0700

    first commit

---
+++ src/athocrestclientest/AtHocRestClienTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package athocrestclientest;
+
+/**
+ *
+ * @author z.alex.kurocha
+ */
+public class AtHocRestClienTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Int().setVisible(true);
+            }
+        });
+    }
+}
